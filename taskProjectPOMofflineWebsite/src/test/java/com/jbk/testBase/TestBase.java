a96003eb9cabbf2bd5c3154d388d5115388341c3
tushr nale <tushar.nale100@gmail.com>
2020-11-05 01:48:52 +0530

    commit

--- taskProjectPOMofflineWebsite/src/test/java/com/jbk/testBase/TestBase.java
+++ taskProjectPOMofflineWebsite/src/test/java/com/jbk/testBase/TestBase.java
@@ -1,5 +1,18 @@
 package com.jbk.testBase;
 
-public class TestBase {
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
+public class TestBase {
+	public static Logger logger;
+ public Logger loggerTest(){
+	 logger	=  Logger.getLogger(Logger.class);
+		String path=(System.getProperty("user.dir")+"log4j.properties");
+		System.out.println(path);
+		PropertyConfigurator.configure("E:\\workspace3\\FinalModuleAddUser\\src\\main\\resources\\log4j.properties");
+		logger.setLevel(Level.INFO);
+		return logger;
+	 
+ }
 }
