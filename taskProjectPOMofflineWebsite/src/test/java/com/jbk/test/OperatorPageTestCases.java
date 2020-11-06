ffa8c534b432353fa8ea38f007e5637d5d41c002
Tejasvini <thshinde1994@gmail.com>
2020-11-06 15:46:56 +0530

    Extent Report for Operator page added by Tejasvini

--- taskProjectPOMofflineWebsite/src/test/java/com/jbk/test/OperatorPageTestCases.java
+++ taskProjectPOMofflineWebsite/src/test/java/com/jbk/test/OperatorPageTestCases.java
@@ -9,21 +9,26 @@ import org.openqa.selenium.WebDriver;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.jbk.pages.Dashboardpage;
 import com.jbk.pages.LoginPageS;
 import com.jbk.pages.OperatorPage;
 import com.jbk.pages.UsefulLinksPage;
+import com.jbk.utility.ExtentReporterNG;
 import com.jbk.utility.OperatorPageDataProvider;
 
 
 
 
 public class OperatorPageTestCases extends BaseTest{
-
+	ExtentReports extent=ExtentReporterNG.getObject("OperatorPage");
+	ExtentTest test;
 	WebDriver wd;
     OperatorPage op;
     Dashboardpage dp;
@@ -34,18 +39,26 @@ public class OperatorPageTestCases extends BaseTest{
 		op=lp.nevigeteToDashboard(wd).navigateToOperatorsPage();
 		op=new OperatorPage(wd);
     }
+	@BeforeClass
+	public void createnode(){
+		test=extent.createTest("Operator  page");
+		test.createNode("Test cases for Operator  page");
+	}
+	
 	@Test(priority=1)
 	public void verifyPageHeading() {
-		
+		ExtentTest test1=test.createNode("verifyPageHeading");
 		Assert.assertTrue(op.isValidHeading());
+		extent.flush();
 	}
 	@Test(priority=2)
 	public void verifyTableHeaderCount() {
-		
+		ExtentTest test1=test.createNode("verifyTableHeaderCount");
 		Assert.assertTrue(op.isHeaderCountValid());
+		extent.flush();
 	}
 	@Test(dataProviderClass=OperatorPageDataProvider.class,dataProvider="getHeaderData",priority=3)
-	public void verifyTableHeader(String id, String person, String reason, String wayOfConnect, String contact,
+	public void verifyTableHeaders(String id, String person, String reason, String wayOfConnect, String contact,
 			String timing) throws Exception {
 		List<String> expHeader = new ArrayList<String>();
 		expHeader.add(id);
@@ -54,44 +67,52 @@ public class OperatorPageTestCases extends BaseTest{
 		expHeader.add(wayOfConnect);
 		expHeader.add(contact);
 		expHeader.add(timing);
-		
+		ExtentTest test1=test.createNode("verifyTableHeaders");
 		Assert.assertEquals(op.getTableHeaders(), expHeader);
+		extent.flush();
 	}
 	@Test(priority=4)
 	public void validateMobileNoSize() {
-		
+		ExtentTest test1=test.createNode("validateMobileNoSize");
 		Assert.assertTrue(op.isMobileNoSizeValid());
+		extent.flush();
 	}
 	
 	@Test(priority=5)
 	public void verifyPageHeader() {
-        
+		ExtentTest test1=test.createNode("verifyPageHeader");
 		Assert.assertTrue(op.isValidPageHeading());
+		extent.flush();
 	}
 	@Test(priority=6)
 	public void verifyTableHeading() {
-		
+		ExtentTest test1=test.createNode("verifyTableHeading");
 		Assert.assertTrue(op.isValidTableHeading());
+		extent.flush();
 	}
 	@Test(priority=7)
 	public void validateTableHeader() throws Exception {
-		
-		Assert.assertTrue(op.isValidTableHeader());
+		ExtentTest test1=test.createNode("validateTableHeader");
+        Assert.assertTrue(op.isValidTableHeader());
+		extent.flush();
 	}
 	@Test(priority=8)
 	public void validateTableData() throws Exception {
-		
+		ExtentTest test1=test.createNode("validateTableData");
 		Assert.assertTrue(op.isValidTableData());
+		extent.flush();
 	}
 	@Test(priority=9)
 	public void validatePersonNames() throws Exception {
-		
+		ExtentTest test1=test.createNode("validatePersonNames");
 		Assert.assertTrue(op.isValidPersonNames());
+		extent.flush();
 	}
 	@Test(priority=10)
 	public void validateTableSize() throws Exception {
-		
+		ExtentTest test1=test.createNode("validateTableSize");
 		Assert.assertTrue(op.isValidTableSize());
+		extent.flush();
 	}
 
 	
