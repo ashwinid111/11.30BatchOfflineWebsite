e32b99ae9c89e0cb825ee4e11e9bba5bc5e32ce8
tushr nale <tushar.nale100@gmail.com>
2020-11-10 10:34:46 +0530

    .

--- taskProjectPOMofflineWebsite/src/main/java/com/jbk/pages/Dashboardpage.java
+++ taskProjectPOMofflineWebsite/src/main/java/com/jbk/pages/Dashboardpage.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+
 import com.jbk.objectRepository.DashboardRepository;
 
 public class Dashboardpage extends DashboardRepository {
 
 	WebDriver driver;
-	int i=1;
+	int i = 1;
+
 	public Dashboardpage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 		this.driver = driver;
 	}
-	
+
 	public boolean checkCourses(WebDriver driver) {
 		List<String> expCourses = new ArrayList<String>();
 		expCourses.add("Selenium");
@@ -29,7 +29,7 @@ public class Dashboardpage extends DashboardRepository {
 		expCourses.add("Php");
 		System.out.println("Expected>>>>>" + expCourses);
 
-		//List<WebElement> actual = driver.findElements(By.xpath("//h3"));
+		// List<WebElement> actual = driver.findElements(By.xpath("//h3"));
 		List<WebElement> actual = Courses;
 		List<String> actCourses = new ArrayList<String>();
 		for (WebElement we : actual) {
@@ -38,10 +38,10 @@ public class Dashboardpage extends DashboardRepository {
 		System.out.println(
 				"---------------------------------------------------------------------------------------------------------------");
 		System.out.println("Actual>>>>>" + actCourses);
-		
-		if(actCourses.equals(expCourses)) {
+
+		if (actCourses.equals(expCourses)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
@@ -54,7 +54,7 @@ public class Dashboardpage extends DashboardRepository {
 		expCourses.add("Web Development");
 		System.out.println("Expected>>>>>" + expCourses);
 
-		List<WebElement> actual =checkSubCourses ;
+		List<WebElement> actual = checkSubCourses;
 		List<String> actCourses = new ArrayList<>();
 		for (WebElement we : actual) {
 			actCourses.add(we.getText());
@@ -62,9 +62,9 @@ public class Dashboardpage extends DashboardRepository {
 		System.out.println(
 				"---------------------------------------------------------------------------------------------------------------");
 		System.out.println("Actual>>>>>" + actCourses);
-		if(actCourses.equals(expCourses)) {
+		if (actCourses.equals(expCourses)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
@@ -73,9 +73,9 @@ public class Dashboardpage extends DashboardRepository {
 		WebElement button = buttonIsClickable;
 		boolean actcheck = button.isEnabled();
 		System.out.println(">>>" + actcheck);
-		if(button.isEnabled()) {
+		if (button.isEnabled()) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
@@ -102,29 +102,29 @@ public class Dashboardpage extends DashboardRepository {
 		}
 		return true;
 	}
-	
+
 	public boolean logoutButtonWorking() {
 		WebElement button = logoutButtonWork;
 		button.click();
-		String actTitle=driver.getTitle();
-		String ExpTitle="JavaByKiran | Log in";
-		if(actTitle.equals(ExpTitle)) {
+		String actTitle = driver.getTitle();
+		String ExpTitle = "JavaByKiran | Log in";
+		if (actTitle.equals(ExpTitle)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public boolean checkDashboardUrl() {
-		String actUrl=driver.getCurrentUrl();
-		String expUrl="file:///F:/Offline%20website/pages/examples/dashboard.html";
-		if(actUrl.equals(expUrl)) {
+		String actUrl = driver.getCurrentUrl();
+		String expUrl = "file:///F:/Offline%20website/pages/examples/dashboard.html";
+		if (actUrl.equals(expUrl)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	/*
 	 * @Test(dataProvider = "dashboardTestData") public void sidebarList(String
 	 * name1,String name2,String name3,String name4,String name5,String name6) {
@@ -141,46 +141,42 @@ public class Dashboardpage extends DashboardRepository {
 	 * ); System.out.println("Actual in method>>>>>"+actSidebar); }
 	 * Assert.assertEquals(actSidebar, expSidebar); }
 	 */
-	
-	public void navigateToUserPage()
-	{
+
+	public AddUser navigateToUserPage() {
 		userPage.click();
+		return new AddUser(driver);
 	}
-	/*@author Tejasvini
-	 *  changing return type of Operators page 
+
+	/*
+	 * @author Tejasvini changing return type of Operators page
 	 * 
 	 * 
 	 */
-	public OperatorPage  navigateToOperatorsPage()
-	{
-		WebElement operator=operatorsPage;
+	public OperatorPage navigateToOperatorsPage() {
+		WebElement operator = operatorsPage;
 		operator.click();
 		return new OperatorPage(driver);
 	}
-	
-	/*@author Mayur khakre
-	 *  changing return type of useful Links page 
+
+	/*
+	 * @author Mayur khakre changing return type of useful Links page
 	 * 
 	 * 
 	 */
-	public UsefulLinksPage navigateToUsefulLinksPage()
-	{
-		WebElement usefulLink=usefulLinksPage;
+	public UsefulLinksPage navigateToUsefulLinksPage() {
+		WebElement usefulLink = usefulLinksPage;
 		usefulLink.click();
 		return new UsefulLinksPage(driver);
 	}
-	
-	public void navigateToDownloadsPage()
-	{
-		WebElement downloads=downloadsPage;
+
+	public void navigateToDownloadsPage() {
+		WebElement downloads = downloadsPage;
 		downloads.click();
 	}
-	
-	public void navigateHomePage()
-	{
-		WebElement logout=logoutPage;
+
+	public void navigateHomePage() {
+		WebElement logout = logoutPage;
 		logout.click();
 	}
-	
-	
+
 }
